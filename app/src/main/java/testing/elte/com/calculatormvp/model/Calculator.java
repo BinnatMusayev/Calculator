c8729e82f866caaf86d5225cc2f66aac7b15785c
Binnat Musayev <binnat.musayev@sap.com>
2019-02-13 23:31:13 +0100

    calculator app with basic functionality

--- app/src/main/java/testing/elte/com/calculatormvp/model/Calculator.java
+++ app/src/main/java/testing/elte/com/calculatormvp/model/Calculator.java
@@ -2,7 +2,9 @@ package testing.elte.com.calculatormvp.model;
 
 public class Calculator {
 
-    private int result;
+    private float firstnum, secondnum, result;
+    private boolean errorOccured, operatorEntered, newNumber;
+    private char operation;
     private String calculationText;
 
     public Calculator(){
@@ -10,19 +12,91 @@ public class Calculator {
         this.calculationText="0";
     }
 
-    public int getResult() {
+    public void calculate(){
+        switch (operation){
+            case '*':
+                result = firstnum * secondnum;
+                break;
+            case '/':
+                if (secondnum == 0 || secondnum == 0.0 ){
+                    errorOccured = true;
+                    result = 0;
+                }else {
+                    result = firstnum / secondnum;
+                }
+                break;
+            case '+':
+                result = firstnum + secondnum;
+                break;
+            case '-':
+                result = firstnum - secondnum;
+                break;
+            default:
+                errorOccured = true;
+        }
+    }
+
+    public float getFirstnum() {
+        return firstnum;
+    }
+
+    public void setFirstnum(float firstnum) {
+        this.firstnum = firstnum;
+    }
+
+    public float getSecondnum() {
+        return secondnum;
+    }
+
+    public void setSecondnum(float secondnum) {
+        this.secondnum = secondnum;
+    }
+
+    public float getResult() {
         return result;
     }
 
-    public void setResult(int result) {
+    public void setResult(float result) {
         this.result = result;
+    }
+
+    public char getOperation() {
+        return operation;
+    }
+
+    public void setOperation(char operation) {
+        this.operation = operation;
     }
 
     public String getCalculationText() {
         return calculationText;
     }
 
+    public void setErrorOccured(boolean errorOccured) {
+        this.errorOccured = errorOccured;
+    }
+
+    public boolean isErrorOccured() {
+        return errorOccured;
+    }
+
     public void setCalculationText(String calculationText) {
         this.calculationText = calculationText;
+    }
+
+    public boolean isOperatorEntered() {
+        return operatorEntered;
+    }
+
+    public void setOperatorEntered(boolean operatorEntered) {
+        this.operatorEntered = operatorEntered;
+    }
+
+    public boolean isNewNumber() {
+        return newNumber;
+    }
+
+    public void setNewNumber(boolean newNumber) {
+        this.newNumber = newNumber;
     }
 }
--- app/src/main/java/testing/elte/com/calculatormvp/presenter/CalculatorPrenseter.java
+++ app/src/main/java/testing/elte/com/calculatormvp/presenter/CalculatorPrenseter.java
@@ -8,36 +8,40 @@ public class CalculatorPrenseter {
 
     Calculator calculator;
     MainView mainView ;
-    private ArrayList<Character> operators;
 
     public CalculatorPrenseter(MainView mainView){
         this.calculator= new Calculator();
         this.mainView = mainView;
-
-        operators = new ArrayList<>();
     }
 
     public void numClicked(int n){
         if (!calculator.getCalculationText().equals("0")
+                && !calculator.isErrorOccured()
                 && !calculator.getCalculationText().equalsIgnoreCase("Infinity")
                 && !calculator.getCalculationText().equalsIgnoreCase("Error")){
 
 
-            if( lastNum().equals("0") && n == 0 && containsOperator()){
+            if(lastNum().equals("0") && n == 0 ){
                 //avaiding adding 0 continuosly
             }else{
-                calculator.setCalculationText(calculator.getCalculationText() + n);
+                if (calculator.isNewNumber()){
+                    calculator.setCalculationText(String.valueOf(n));
+                    calculator.setNewNumber(false);
+                }else {
+                    calculator.setCalculationText(calculator.getCalculationText() + n);
+                }
             }
         } else {
                 calculator.setCalculationText(String.valueOf(n));
         }
 
+        calculator.setErrorOccured(false);
         mainView.setText(calculator.getCalculationText());
 
     }
 
     public void dotClicked(){
-        if (!lastNumContainsDot() && !isLastCharOperator()){
+        if (!lastNumContainsDot() ){
                 //everything okay
                 calculator.setCalculationText(calculator.getCalculationText() + ".");
                 mainView.setText(calculator.getCalculationText());
@@ -45,38 +49,53 @@ public class CalculatorPrenseter {
     }
 
     public void operatorClicked(String operator){
-        if (isLastCharOperator() ){
-            calculator.setCalculationText(
-                    calculator.getCalculationText().substring(0, calculator.getCalculationText().length()-1) + operator);
+        if (calculator.isOperatorEntered()){
+            calculateWithoutOperation();
+            calculator.setFirstnum(calculator.getResult());
+            mainView.setText(String.valueOf(calculator.getResult()));
+
         }else {
-            calculator.setCalculationText(calculator.getCalculationText() + operator);
+            calculator.setFirstnum(Float.valueOf(calculator.getCalculationText()));
         }
-        mainView.setText(calculator.getCalculationText());
+        calculator.setOperation(operator.charAt(0));
+        calculator.setNewNumber(true);
+        calculator.setOperatorEntered(true);
+
     }
 
     public void clear(){
+        calculator.setFirstnum(0);
+        calculator.setSecondnum(0);
+        calculator.setResult(0);
+        calculator.setOperatorEntered(false);
+        calculator.setNewNumber(false);
         calculator.setCalculationText("0");
         mainView.clearTextView();
     }
 
     public void calculate(){
-        if (!isLastCharOperator() || calculator.getCalculationText().endsWith(".")){
-//            mainView.setText(lastNum());
-            generateOperatorsArray();
-//            try {
-                eliminateHigherOrder();
-                finalizeCalculation();
-//            }catch (Exception e){
-//                mainView.setText("Error");
-//                e.printStackTrace();
-//            }
-//            mainView.setText(operators.toString());
+        calculator.setSecondnum(Float.valueOf(calculator.getCalculationText()));
+        calculator.calculate();
+        if (calculator.isErrorOccured()){
+            mainView.setText("Error");
+        }else{
+            mainView.setText(String.valueOf(calculator.getResult()));
+            calculator.setFirstnum(calculator.getResult());
         }
+        calculator.setCalculationText(String.valueOf(calculator.getResult()));
+        calculator.setOperatorEntered(false);
     }
 
-    public boolean isLastCharOperator(){
-        char lastChar = calculator.getCalculationText().charAt(calculator.getCalculationText().length()-1);
-        return lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/' || lastChar == '.' ;
+    public void calculateWithoutOperation(){
+        calculator.setSecondnum(Float.valueOf(calculator.getCalculationText()));
+        calculator.calculate();
+        if (calculator.isErrorOccured()){
+            mainView.setText("Error");
+        }else{
+            mainView.setText(String.valueOf(calculator.getResult()));
+            calculator.setFirstnum(calculator.getResult());
+        }
+        calculator.setCalculationText(String.valueOf(calculator.getResult()));
     }
 
     public boolean lastNumContainsDot(){
@@ -97,6 +116,7 @@ public class CalculatorPrenseter {
         return lastNumBuilder.toString();
     }
 
+    //to get the last number of given string
     public String lastNum(String str){
         StringBuilder lastNumBuilder = new StringBuilder();
         for(int i=str.length()-1; i>=0; i--){
@@ -110,6 +130,7 @@ public class CalculatorPrenseter {
         return lastNumBuilder.toString();
     }
 
+    //to get the first number of given string
     public String firstNum(String str){
         StringBuilder lastNumBuilder = new StringBuilder();
         for(int i=0; i<str.length(); i++){
@@ -123,27 +144,6 @@ public class CalculatorPrenseter {
         return lastNumBuilder.toString();
     }
 
-    public boolean containsOperator(){
-        String str = calculator.getCalculationText();
-//        return str.contains("+") || str.contains("-") || str.contains("*") || str.contains("/");
-        return operators.size()>0;
-    }
-
-    public boolean containsHigherOrderOperator(){
-        String str = calculator.getCalculationText();
-        return str.contains("*") || str.contains("/");
-    }
-
-    public boolean containsMultiplication(){
-        String str = calculator.getCalculationText();
-        return str.contains("*") ;
-    }
-
-    public boolean containsDivision(){
-        String str = calculator.getCalculationText();
-        return str.contains("/");
-    }
-
     public void deleteLast(){
         String str = calculator.getCalculationText();
         if (str.length() == 1){
@@ -155,159 +155,7 @@ public class CalculatorPrenseter {
         mainView.setText(calculator.getCalculationText());
     }
 
-    public void generateOperatorsArray(){
-        operators.clear();
-        for(int i=0; i<calculator.getCalculationText().length(); i++){
-            char c = calculator.getCalculationText().charAt(i);
-            if(c == '+' || c == '-' ||  c == '*' || c == '/' ){
-                operators.add(c);
-            }
-        }
-    }
 
-    private void eliminateHigherOrder() {//throws Exception{
-        while(containsHigherOrderOperator()){
-            float  result;
-            Number firstNum, secondNum;
-            String calcText = calculator.getCalculationText();
-            char firstHigherOrderOperation = getFirstHigherOrder();
-            String firstNumString;
-            if(!calcText.startsWith("-")) {
-                firstNumString = lastNum(calcText.substring(0, calcText.indexOf(String.valueOf(firstHigherOrderOperation))));
-            }else{
-                if (operators.size() <= 2) {
-                    firstNumString = "-" + lastNum(calcText.substring(1, calcText.indexOf(String.valueOf(firstHigherOrderOperation))));
-                }else{
-                    firstNumString = lastNum(calcText.substring(0, calcText.indexOf(String.valueOf(firstHigherOrderOperation))));
-                }
-            }
-
-            if(firstNumString.contains(".")){
-                firstNum = Float.valueOf(firstNumString);
-            }else{
-                firstNum = Integer.valueOf(firstNumString);
-            }
-
-            String secondNumString = firstNum(calcText.substring(calcText.indexOf(String.valueOf(firstHigherOrderOperation))+1));
-            if(secondNumString.contains(".")){
-                secondNum = Float.valueOf(secondNumString);
-            }else{
-                secondNum = Integer.valueOf(secondNumString);
-            }
-
-
-            if(firstHigherOrderOperation == '*'){
-                result = Float.parseFloat(String.valueOf(firstNum))*Float.parseFloat(String.valueOf(secondNum));
-            }else{
-                result = Float.parseFloat(String.valueOf(firstNum))/Float.parseFloat(String.valueOf(secondNum));
-            }
-
-            String oldSubString = String.valueOf(firstNum)+ String.valueOf(firstHigherOrderOperation) + String.valueOf(secondNum);
-            calculator.setCalculationText(calculator.getCalculationText().replace(oldSubString, String.valueOf(result)) );
-            if (!containsOperator()) {
-                mainView.setText(calculator.getCalculationText());
-            }
-            operators.remove(operators.indexOf(getFirstHigherOrder()));
-            if(calcText.startsWith("-") && operators.size() <=2) {
-                operators.remove(0);
-            }
-            mainView.setText(String.valueOf(result));
-
-        }
-
-        //for cases like 2*5+2*5
-        while (operators.contains('*') || operators.contains('/')){
-            operators.remove(operators.indexOf(getFirstHigherOrder()));
-        }
-    }
-
-    public void finalizeCalculation(){
-        while(containsOperator()) {
-            float result;
-            Number firstNum, secondNum;
-            String calcText = calculator.getCalculationText();
-//            char firstOperation = getFirstOperator();
-            char firstOperation;
-            if(calcText.startsWith("-")) {
-                firstOperation = operators.get(1);
-            }else{
-                firstOperation = operators.get(0);
-            }
-            //check if first number is negative or not
-            String firstNumString;
-            if(!calcText.startsWith("-")) {
-                 firstNumString = lastNum(calcText.substring(0, calcText.indexOf(String.valueOf(firstOperation))));
-            }else{
-                firstNumString = "-" + lastNum(calcText.substring(1, calcText.indexOf(String.valueOf(firstOperation))));;
-            }
-            if (firstNumString.contains(".")) {
-                firstNum = Float.valueOf(firstNumString);
-            } else {
-                firstNum = Integer.valueOf(firstNumString);
-            }
-
-            String secondNumString;
-            if (operators.size() == 1 || (operators.size() == 2 && calcText.startsWith("-")) ) {
-                secondNumString = lastNum(calcText.substring(calcText.indexOf(String.valueOf(firstOperation)) + 1));
-            }else {
-                secondNumString = firstNum(calcText.substring(calcText.indexOf(String.valueOf(firstOperation)) + 1));
-            }
-
-            if (secondNumString.contains(".")) {
-                secondNum = Float.valueOf(secondNumString);
-            } else {
-                secondNum = Integer.valueOf(secondNumString);
-            }
-
-
-            if (firstOperation == '+') {
-                result = Float.parseFloat(String.valueOf(firstNum)) + Float.parseFloat(String.valueOf(secondNum));
-            } else {
-                result = Float.parseFloat(String.valueOf(firstNum)) - Float.parseFloat(String.valueOf(secondNum));
-            }
-
-            String oldSubString = String.valueOf(firstNum) + String.valueOf(firstOperation) + String.valueOf(secondNum);
-            calculator.setCalculationText(calculator.getCalculationText().replace(oldSubString, String.valueOf(result)));
-            mainView.setText(calculator.getCalculationText());
-            operators.remove(0);
-            //because previousle it removed negative sign
-            if(calcText.startsWith("-") && operators.size() <=2) {
-                operators.remove(0);
-            }
-//            mainView.setText(String.valueOf(result));
-
-        }
-    }
-
-    private char getFirstHigherOrder(){
-        for (char c: operators){
-            if (c == '*' || c == '/'){
-                return c;
-            }
-        }
-        return '*';
-    }
-
-    private char getFirstOperator(){
-//        for (char c: operators){
-//            if (c == '+' || c == '-'){
-//                return c;
-//            }
-//        }
-        for (int i =0; i< operators.size(); i++){
-            if (operators.get(i) == '+' || operators.get(i) == '-'){
-                //if negative then skip
-                if (i==0) {
-                    if (!calculator.getCalculationText().startsWith("-") ) {
-                        return operators.get(i);
-                    }
-                }else{
-                    return operators.get(i);
-                }
-            }
-        }
-        return '+';
-    }
 
 
     //interface for view
